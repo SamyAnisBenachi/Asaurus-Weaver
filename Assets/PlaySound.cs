3f4810e9735c6d42c8a47225d1c2982a74534bb0
SamyAnisBenachi <51319830+SamyAnisBenachi@users.noreply.github.com>
2020-04-29 01:38:15 -0700

    Add sound scripts but not sound effect

--- Assets/PlaySound.cs
+++ Assets/PlaySound.cs
@@ -4,30 +4,88 @@ using UnityEngine;
 
 public class PlaySound : MonoBehaviour
 {
+    public AudioSource audioSource;
+
     public AudioClip footStepLeft;
     public AudioClip footStepRight;
 
-    public AudioSource audioSource;
+    public AudioClip getHit;
+    public AudioClip die;
 
-    void Start()
-    {
+    public AudioClip attack1;
+    public AudioClip attack2;
+    public AudioClip attack3;
+    public AudioClip attack4;
 
-    }
-
-    void Update()
-    {
-
-    }
+    public AudioClip defendEffect;
+    public AudioClip swordEffect;
 
     void SoundFootstepLeft()
     {
-        audioSource.PlayOneShot(footStepLeft, 0.1f);
-        Debug.Log("test L ");
+        if (footStepLeft)
+            audioSource.PlayOneShot(footStepLeft, 0.1f);
+        Debug.Log("SoundFootstepLeft");
     }
 
     void SoundFootstepRight()
     {
-        audioSource.PlayOneShot(footStepRight, 0.1f);
-        Debug.Log("test R");
+        if (footStepRight)
+            audioSource.PlayOneShot(footStepRight, 0.1f);
+        Debug.Log("SoundFootstepRight");
+    }
+
+    void SoundAttack1()
+    {
+        if (attack1)
+            audioSource.PlayOneShot(attack1, 1f);
+        Debug.Log("SoundAttack1");
+    }
+
+    void SoundAttack2()
+    {
+        if (attack2)
+            audioSource.PlayOneShot(attack2, 1f);
+        Debug.Log("SoundAttack2");
+    }
+
+    void SoundAttack3()
+    {
+        if (attack3)
+            audioSource.PlayOneShot(attack3, 1f);
+        Debug.Log("SoundAttack3");
+    }
+
+    void SoundAttack4()
+    {
+        if (attack4)
+            audioSource.PlayOneShot(attack4, 1f);
+        Debug.Log("PlayOneShot");
+    }
+
+    void SoundAuraShield()
+    {
+        if (defendEffect)
+            audioSource.PlayOneShot(defendEffect, 1f);
+        Debug.Log("SoundAuraShield");
+    }
+    void SoundAuraSword()
+    {
+        if (swordEffect)
+            audioSource.PlayOneShot(swordEffect, 1f);
+        Debug.Log("SoundAuraSword");
+    }
+
+    void SoundGetHit()
+    {
+        if (getHit)
+            audioSource.PlayOneShot(getHit, 1f);
+        Debug.Log("SoundgetHit");
+    }
+
+    void SoundDie()
+    {
+        if (getHit)
+            audioSource.PlayOneShot(die, 1f);
+        Debug.Log("SoundDie");
     }
 }
---
+++ Assets/PlaySoundEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundEnemy : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip attack;
+    public AudioClip getHit;
+    public AudioClip die;
+
+    void SoundAttack1()
+    {
+        if (attack)
+            audioSource.PlayOneShot(attack, 1f);
+        Debug.Log("SoundAttack");
+    }
+
+    void SoundGetHit()
+    {
+        if (getHit)
+            audioSource.PlayOneShot(getHit, 1f);
+        Debug.Log("SoundgetHit");
+    }
+
+    void SoundDie()
+    {
+        if (die)
+            audioSource.PlayOneShot(die, 1f);
+        Debug.Log("SoundDie");
+    }
+}
