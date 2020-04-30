c314a4b3b1cd452b26b4d43a6214b2bd96f044ea
SamyAnisBenachi <51319830+SamyAnisBenachi@users.noreply.github.com>
2020-04-30 13:51:04 -0700

    UI look at the player

---
+++ Assets/LookAtPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    Camera cameraToLook;
+
+    void Start()
+    {
+        cameraToLook = Camera.main;
+    }
+
+    void Update()
+    {
+        // UI Look at the player
+        transform.LookAt(transform.position + cameraToLook.transform.rotation * Vector3.back, cameraToLook.transform.rotation * Vector3.up);
+    }
+}
